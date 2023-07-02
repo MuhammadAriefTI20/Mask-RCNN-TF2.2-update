336144427d0234fb7b2dcc63cd9e87d4c2a87657
Muhammad Arief <93487560+MuhammadAriefTI20@users.noreply.github.com>
2023-07-02 14:26:14 +0900

    Create cek.py

---
+++ cek.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import numpy as np
+import keras as kr
+
+print("versi Tensorflow = "+tf.version.VERSION)
+print("versi Numpy = "+np.version.version)
+"""
+hanya untuk cek versi tensorflow dan numpy yang harusnya sudah sesuai lib yang dibutuhkan
+#M.A
+"""

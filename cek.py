b05f65b605b822b2d0faa2bf6ed495ef7a8b7cab
Muhammad Arief <93487560+MuhammadAriefTI20@users.noreply.github.com>
2023-07-03 00:09:09 +0900

    Update cek.py

--- cek.py
+++ cek.py
@@ -4,7 +4,11 @@ import keras as kr
 
 print("versi Tensorflow = "+tf.version.VERSION)
 print("versi Numpy = "+np.version.version)
+if len(tf.config.list_physical_devices('GPU')) > 0:    
+    print("You have a GPU enabled.")
+else:    
+    print("Enable a GPU before running this notebook.")
 """
-hanya untuk cek versi tensorflow dan numpy yang harusnya sudah sesuai lib yang dibutuhkan
+hanya untuk cek versi tensorflow, numpy, dan gpu harusnya sudah sesuai lib yang dibutuhkan
 #M.A
 """
